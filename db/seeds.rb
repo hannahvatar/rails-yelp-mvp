8ccc0aeb359462f4cd325cf7c393124cf9f96820
Anabelle Dubois <anabelle.dubois@icloud.com>
2024-11-20 15:30:10 -0500

    Seed done

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,30 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Restaurant.create!([
+  {
+    name: 'Grillade La Bohème',
+    address: '955 Rue Richelieu, La Malbaie',
+    category: 'french',
+  },
+  {
+    name: 'Restaurant Allegro',
+    address: '990 Rue Richelieu, La Malbaie',
+    category: 'belgian',
+  },
+  {
+    name: 'Bistro Local 245',
+    address: '245 Bd de Comporté, La Malbaie',
+    category: 'french',
+  },
+  {
+    name: 'Café De La Gare',
+    address: '100 Chem. du Havre, La Malbaie',
+    category: 'italian',
+  },
+  {
+    name: 'Île Mystérieuse',
+    address: '1055 Rue Richelieu, La Malbaie',
+    category: 'japanese',
+  }
+])
